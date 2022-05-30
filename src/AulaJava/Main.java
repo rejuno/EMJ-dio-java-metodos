e102a7eee789c0cb2d5c54d75b4449a414e39a98
Renata Novais <98842524+rejuno@users.noreply.github.com>
2022-05-30 13:48:17 -0300

    Update Main.java

--- src/AulaJava/Main.java
+++ src/AulaJava/Main.java
@@ -4,14 +4,50 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		//Aqui vai executar!
+		Scanner r = new Scanner(System.in);
+		
+		//CALCULO
+		
+		System.out.println("Digite o primeiro número: ");
+		double a = r.nextDouble();
+		System.out.println("Digite o segundo número: ");
+		double b = r.nextDouble();
+		
+		System.out.println("Resultados");
+		
+		System.out.println("Soma");
+		System.out.println(Calculo.soma(a, b));
+		
+		System.out.println("Subtração");
+		System.out.println(Calculo.subtracao(a, b));
+		
+		System.out.println("Multiplicação");
+		System.out.println(Calculo.multiplicacao(a, b));
+		
+		System.out.println("Divisão");
+		System.out.println(Calculo.divisao(a, b));
+
+		//HORA
+		Scanner e = new Scanner(System.in);
+		
+		System.out.println("Que horas são? ");
+		int hora = e.nextInt();
+		Hora.mensagem(hora);
+		
+		//EMPRESTIMO
+		System.out.println("Exercício empréstimo");
+		Emprestimo.calculo(1000, Emprestimo.DuasParcelas());
+		Emprestimo.calculo(1000, Emprestimo.TresParcelas());
+		Emprestimo.calculo(1000, 5);
 		
 		//SOBRECARGA
-        Scanner s = new Scanner(System.in);
-        
-        System.out.println("O que deseja calcular? 1.Quadrado 2.Retangulo 3.Trap�zio");
-        int opcao = s.nextInt();
-        Sobrecarga.opcao(opcao);
-        
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("O que deseja calcular? 1.Quadrado 2.Retangulo 3.Trapézio");
+		int opcao = s.nextInt();
+		Sobrecarga.opcao(opcao);
+
         
 	}
 
