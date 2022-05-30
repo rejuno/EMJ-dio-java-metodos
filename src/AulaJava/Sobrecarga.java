2b82967fc5bbff36af4bb8bdeb4f9816817d51ec
Renata Novais <98842524+rejuno@users.noreply.github.com>
2022-05-30 13:46:12 -0300

    Exercicio BootCamp Java Carrefour(dio) - Sobrecarga

---
+++ src/AulaJava/Sobrecarga.java
@@ -0,0 +1,67 @@
+package AulaJava;
+import java.util.Scanner;
+import java.lang.System;
+public class Sobrecarga {
+	//CONDICAO
+	public static void opcao(int opcao) {
+		
+		Scanner l = new Scanner(System.in);
+		switch(opcao) {
+		case 1:
+			
+			System.out.println("Digite o valor do lado: ");
+			int lado = l.nextInt();
+			
+			quadrado(lado);
+			break;
+		
+		case 2:
+			
+			System.out.println("Digite o valor do primeiro lado:");
+			int lado1 = l.nextInt();
+		
+			System.out.println("Digite o valor do segundo lado:");
+			int lado2 = l.nextInt();
+			
+			retangulo(lado1, lado2);
+			break;
+		
+		case 3:
+			
+			System.out.println("Digite o valor da base menor: ");
+			int menor = l.nextInt();
+			
+			System.out.println("Digite o valor da base maior: ");
+			int maior = l.nextInt();
+			
+			System.out.println("Digite o valor da altura: ");
+			int h = l.nextInt();
+			
+			trapezio(menor, maior, h);
+			break;
+		default:
+			
+			System.out.println("Opção Inválida");
+			break;
+		}
+		
+	}
+	public static void retangulo(int lado1, int lado2) {
+		int resultador = lado1*lado2;
+		System.out.println(resultador);
+		
+	}
+	public static void quadrado(int lado) {
+		
+		int resultadoq = lado*lado;
+		System.out.println(resultadoq);
+		
+		
+	}
+	public static void trapezio(int menor, int maior, int h) {
+		int resultadot = (maior+menor)*h/2;
+		System.out.println(resultadot);
+	
+	}
+	
+}
